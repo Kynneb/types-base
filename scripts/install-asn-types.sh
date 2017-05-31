212d24e1f42e125697a265a643e8ac4a010babcc
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2017-05-31 15:32:58 +0200

    changed path order

--- scripts/install-asn-types.sh
+++ scripts/install-asn-types.sh
@@ -6,7 +6,14 @@ if [ "$#" -lt 2 ]; then
 fi
 
 dataViewAADL=${3}".aadl"
-dataViewAADL=${2}${dataViewAADL} 
+dataViewAADL="${2}/${dataViewAADL}"
+
+echo "name: $0"
+echo "asn : $1"
+echo "out : $2"
+echo "onam: $3"
+echo $dataViewAADL
+
 
 asn2aadlPlus \
 `find "$1" -maxdepth 1 -name "*.asn" -printf '%p '`\
