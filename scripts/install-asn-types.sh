0130219f31ef4a030e125cf4d35ac6705b0ae320
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2017-06-02 15:18:02 +0200

    removed debug echos

--- scripts/install-asn-types.sh
+++ scripts/install-asn-types.sh
@@ -8,12 +8,6 @@ fi
 dataViewAADL=${3}".aadl"
 dataViewAADL="${2}/${dataViewAADL}"
 
-echo "name: $0"
-echo "asn : $1"
-echo "out : $2"
-echo "onam: $3"
-echo $dataViewAADL
-
 asn2aadlPlus \
 `find "$1" -maxdepth 1 -name "*.asn" -printf '%p '`\
 `find "$1" -maxdepth 1 -name "*.acn" -printf '%p '`\
